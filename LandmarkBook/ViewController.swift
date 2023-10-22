b64e7b116ed518fca7a26aad040610dc9ef620bd
İsmail Harmanda <iharmanda@hotmail.com>
2023-10-22 22:32:26 +0300

    initial commit

---
+++ LandmarkBook/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  LandmarkBook
+//
+//  Created by ismail harmanda on 22.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var tasbleView: UITableView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
